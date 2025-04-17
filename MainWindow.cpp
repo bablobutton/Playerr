b6ca90142fd6d06a6b61e7c13215f7e5aadbd035
Константин Даль <djkostjan@MacBook-Pro-Konstantin.local>
2025-04-17 15:34:35 +0300

    init

---
+++ MainWindow.cpp
@@ -0,0 +1,25 @@
+#include "MainWindow.hpp"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    // Размерчик
+    resize(400, 300);
+    
+    // Кнопочка
+    m_button = new QPushButton("BABLO", this);
+    m_button->setGeometry(150, 120, 100, 30);
+    
+    // Подключение сигнала к слоту
+    connect(m_button, &QPushButton::clicked, this, &MainWindow::onButtonClicked);
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::onButtonClicked()
+{
+    QMessageBox::information(this, "Сообщение", "BABLO BABLO!");
+}
---
+++ MainWindow.hpp
@@ -0,0 +1,22 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onButtonClicked();
+
+private:
+    QPushButton *m_button;
+};
+
+#endif // MAINWINDOW_H
